c89296f8c7887649e07e3b16a432213b5bd0658b
Vikas <vikas.vyas@aol.com>
2019-11-19 22:40:48 -0500

    third commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-Local-mongoose');
+
+const userSchema = new Schema({
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
