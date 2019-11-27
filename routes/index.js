5558f738bfc930c0f43d7152e89bc74677e1bed4
vikasvyas237 <50597541+vikasvyas237@users.noreply.github.com>
2019-11-26 22:58:02 -0500

    sixth commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. from the router function */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
